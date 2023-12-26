cbcf7e85ed1aa8d906e09ccb90c426b938c8a4ee
mary <mashatyu@mail.ru>
2023-12-26 23:10:48 +0000

    create EndMoveCommand, EndMoveCommandTest (.cs)

---
+++ SpaceBattle.Lib/EndMoveCommand.cs
@@ -0,0 +1,21 @@
+﻿using Hwdtech;
+
+namespace SpaceBattle.Lib;
+
+public class EndMoveCommand : ICommand
+{
+    private readonly IMoveCommandEndable _end;
+
+    public EndMoveCommand(IMoveCommandEndable end)
+    {
+        _end = end;
+    }
+
+    public void Execute()
+    {
+        var emptyCmd = IoC.Resolve<ICommand>("Cmd.Empty");
+        _end.Property.ToList().ForEach(value => IoC.Resolve<ICommand>("DelProperty", _end.Object, value).Execute());
+        var cmd = IoC.Resolve<IBridgeCommand>("GetProperty", _end.Object, _end.Command);
+        IoC.Resolve<IBridgeCommand>("Cmd.Inject", cmd, emptyCmd);
+    }
+}
