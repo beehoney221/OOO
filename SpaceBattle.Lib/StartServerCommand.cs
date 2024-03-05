6aa970aef2af17613113c84e33db6b54f4bf6229
beehoney221 <mashatyu@mail.ru>
2024-03-06 01:36:53 +0600

    create Start/Stop ServerCommand (.cs)

---
+++ SpaceBattle.Lib/StartServerCommand.cs
@@ -0,0 +1,28 @@
+﻿using Hwdtech;
+
+namespace SpaceBattle.Lib;
+
+public class StartServerCommand : ICommand
+{
+    public void Execute()
+    {
+        IoC.Resolve<Hwdtech.ICommand>(
+        "IoC.Register",
+        "Server.Start.Cmd",
+        (int numThreads) =>
+        {
+            var i = 1;
+            var barrier = new Barrier(numThreads);
+            while (i <= numThreads)
+            {
+                var thread = new Thread(() => { });
+                thread.Start();
+                barrier.SignalAndWait();
+            }
+
+            barrier.SignalAndWait();
+            return "\nУспешный запуск сервера!";
+        }
+        ).Execute();
+    }
+}
---
+++ SpaceBattle.Lib/StopServerCommand.cs
@@ -0,0 +1,26 @@
+﻿using Hwdtech;
+
+namespace SpaceBattle.Lib;
+
+public class StopServerCommand : ICommand
+{
+    public void Execute()
+    {
+        IoC.Resolve<Hwdtech.ICommand>(
+        "IoC.Register",
+        "Server.Stop.Cmd",
+        (int numThreads) =>
+        {
+            var i = 1;
+            Console.Write("Началась процедура остановки.");
+            while (i <= numThreads)
+            {
+
+            }
+
+            var barrier = new Barrier(numThreads);
+            return "\nПроцедура остановки завершена!";
+        }
+        ).Execute();
+    }
+}
