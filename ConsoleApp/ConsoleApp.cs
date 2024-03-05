9f64ac268f7de1cd51121c62c15b0b60959674a5
beehoney221 <mashatyu@mail.ru>
2024-03-06 01:32:49 +0600

    create ConsoleApp add CA reference SBL add Hwdtech.IoC add Hwdtech.Ioc.ScopeBasedIoCImplementation

---
+++ ConsoleApp/ConsoleApp.cs
@@ -0,0 +1,17 @@
+﻿using Hwdtech;
+
+namespace ConsoleApp
+{
+    public class ConsoleApp
+    {
+        public static void Main()
+        {
+            Console.Write("Введите количество потоков: ");
+            var num = Convert.ToInt32(Console.ReadLine());
+            var start = IoC.Resolve<string>("Server.Start.Cmd", num);
+            Console.WriteLine(start);
+            Console.ReadLine();
+            IoC.Resolve<string>("Server.Stop.Cmd", num);
+        }
+    }
+}
