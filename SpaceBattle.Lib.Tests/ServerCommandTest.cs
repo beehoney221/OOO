1d2a9fab22cbf193e5660b11aa87dce16dcf36fc
beehoney221 <mashatyu@mail.ru>
2024-03-20 01:21:08 +0600

    modified exception.message

--- SpaceBattle.Lib.Tests/ServerCommandTest.cs
+++ SpaceBattle.Lib.Tests/ServerCommandTest.cs
@@ -73,7 +73,7 @@ public class ServerCommandTest
     [Fact]
     public void LogStrategySuccesful()
     {
-        var exc = new Exception("Threads' transmitted and expected ID and not coincides.");
+        var exc = new Exception("Threads' transmitted and expected ID are not coincides.");
         var path = Path.GetTempFileName();
 
         new StrategyLog(path).Execute();
