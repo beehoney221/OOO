36b75ed2a86db8bda371548cc4d31fbc7c93f8fc
beehoney221 <mashatyu@mail.ru>
2024-03-26 23:30:24 +0600

    create ConsoleApp

---
+++ ConsoleApp/ConsoleApp.cs
@@ -0,0 +1,22 @@
+﻿﻿using Hwdtech;
+
+namespace SpaceBattle.Lib;
+
+public class ConsoleApp
+{
+    public static void Main()
+    {
+        new RegistStartServerCommand().Execute();
+        new RegistStopServerCommand().Execute();
+
+        Console.WriteLine("Введите количество потоков: ");
+        var num = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Процедура запуска сервера началась.");
+        IoC.Resolve<ICommand>("Server.Start.Cmd", num).Execute();
+        Console.WriteLine("Сервер успешно запущен.");
+        Console.ReadLine();
+        Console.WriteLine("Процедура остановки сервера началась.");
+        IoC.Resolve<ICommand>("Server.Stop.Cmd", num).Execute();
+        Console.WriteLine("Сервер успешно остановлен.");
+    }
+}
