4fb3937dfdbcec8d60e7a53b7ec3bcbfd95ccd3f
beehoney221 <mashatyu@mail.ru>
2024-04-25 00:49:37 +0600

    chore: class --> interface (IContract)

--- SpaceBattle.Lib/Contract.cs
+++
@@ -1,7 +0,0 @@
-﻿public class Contract
-{
-    public string? type { get; set; }
-    public string? gameId { get; set; }
-    public int gameItemId { get; set; }
-    public Dictionary<string, object>? parameters;
-}
---
+++ SpaceBattle.Lib/IContract.cs
@@ -0,0 +1,9 @@
+﻿namespace SpaceBattle.Lib;
+
+public interface IContract
+{
+    public string type { get; }
+    public string gameId { get; }
+    public int gameItemId { get; }
+    public IDictionary<string, object> parameters { get; }
+}
