85e994820e65e56eff9ee6c107122c6e51d99f76
beehoney221 <125187667+beehoney221@users.noreply.github.com>
2024-06-07 11:21:47 +0000

    feat: added startMC (strategy)

---
+++ SpaceBattle.Lib/RegistStartMoveCommand.cs
@@ -0,0 +1,23 @@
+﻿using Hwdtech;
+
+namespace SpaceBattle.Lib;
+
+public class RegistStartMoveCommand : ICommand
+{
+    public void Execute()
+    {
+        IoC.Resolve<Hwdtech.ICommand>(
+            "IoC.Register",
+            "Game.Command.StartMove",
+            (object[] args) =>
+            {
+                var obj = args[0];
+                var parameters = (IDictionary<string, object>)args[1];
+                
+                var cmd = IoC.Resolve<ICommand>("Command.StartMove", obj, parameters);
+                
+                return cmd;
+            }
+        ).Execute();
+    }
+}
